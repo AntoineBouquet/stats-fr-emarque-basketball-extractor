cb10a8a9a49f9af869ce442199534bf47759fdea
AntoineBouquet <bouquetant@gmail.com>
2020-10-31 10:01:08 +0100

    fix canceled history match event, event type get from the last message from it

--- app/services/history-extractor.service.js
+++ app/services/history-extractor.service.js
@@ -5,7 +5,7 @@ const Event = require('../models/basketball/event.model');
 function HistoryExtractor() {}
 
 const processEvent = (event) => {
-  if(event.message.includes("annulé")) {
+  if(event.message.includes("Evénement annulé")) {
     event.canceled = true;
 
     while(event.message.indexOf("Evénement annulé : /") !== -1) {
@@ -17,47 +17,52 @@ const processEvent = (event) => {
     }
   }
 
-  if(event.message.includes("Début du match")) {
+  let message = event.message;
+  if(event.canceled) {
+    message = message.substring(message.indexOf("Remplacé par :"), message.length - 1);
+  }
+
+  if(message.includes("Début du match")) {
     event.eventType = EventType.START_MATCH;
-  } else if(event.message.includes("Tir à 2 points réussi") || event.message.includes("Tir à 3 points réussi") ||
-    event.message.includes("Lancer franc réussi") || event.message.includes("Lancer franc manqué")) {
-    if(event.message.includes("Tir à 2 points réussi")) {
+  } else if(message.includes("Tir à 2 points réussi") || message.includes("Tir à 3 points réussi") ||
+    message.includes("Lancer franc réussi") || message.includes("Lancer franc manqué")) {
+    if(message.includes("Tir à 2 points réussi")) {
       event.eventType = EventType.TWO_POINTS;
-    } else if(event.message.includes("Tir à 3 points réussi")) {
+    } else if(message.includes("Tir à 3 points réussi")) {
       event.eventType = EventType.THREE_POINTS;
-    } else if(event.message.includes("Lancer franc réussi")) {
+    } else if(message.includes("Lancer franc réussi")) {
       event.eventType = EventType.FREE_THROW_SUCCESS;
-    } else if(event.message.includes("Lancer franc manqué")) {
+    } else if(message.includes("Lancer franc manqué")) {
       event.eventType = EventType.FREE_THROW_FAIL;
     }
-    event.from = event.message.substring(1, event.message.indexOf(','));
-    let value = event.message.split(' ').splice(-1)[0];
+    event.from = message.substring(1, message.indexOf(','));
+    let value = message.split(' ').splice(-1)[0];
     event.value = value.substring(1, value.length - 1);
-  } else if(event.message.includes("est entré sur le terrain") || event.message.includes("est entrée sur le terrain") ||
-    event.message.includes("est sorti du terrain") || event.message.includes("est sortie du terrain")) {
-    if(event.message.includes("sur le terrain")) {
+  } else if(message.includes("est entré sur le terrain") || message.includes("est entrée sur le terrain") ||
+    message.includes("est sorti du terrain") || message.includes("est sortie du terrain")) {
+    if(message.includes("sur le terrain")) {
       event.eventType = EventType.PLAYER_ENTERING_COURT;
-    } else if(event.message.includes("du terrain")) {
+    } else if(message.includes("du terrain")) {
       event.eventType = EventType.PLAYER_EXITING_COURT;
     }
 
-    event.from = event.message.substring(1, event.message.indexOf(','));
-  } else if(event.message.includes("Sens initial de la flèche")) {
+    event.from = message.substring(1, message.indexOf(','));
+  } else if(message.includes("Sens initial de la flèche")) {
     event.eventType = EventType.POSSESSION_ARROW_INITIAL;
-    event.value = event.message.substr(event.message.length - 1, 1);
-  } else if(event.message.includes("Flèche retournée")) {
+    event.value = message.substr(message.length - 1, 1);
+  } else if(message.includes("Flèche retournée")) {
     event.eventType = EventType.POSSESSION_ARROW_SET;
-    event.value = event.message.substr(25, 1);
-  } else if(event.message.includes("Faute :")) {
+    event.value = message.substr(25, 1);
+  } else if(message.includes("Faute :")) {
     event.eventType = EventType.PERSONAL_FOUL;
-    event.from = event.message.substring(event.message.indexOf('à') + 3, event.message.indexOf(','));
-    event.value = event.message.substring(event.message.indexOf('Faute : ') + 8, event.message.indexOf('à') - 1);
-  } else if(event.message.includes("Temps-Mort")) {
+    event.from = message.substring(message.indexOf('à') + 3, message.indexOf(','));
+    event.value = message.substring(message.indexOf('Faute : ') + 8, message.indexOf('à') - 1);
+  } else if(message.includes("Temps-Mort")) {
     event.eventType = EventType.TIMEOUT;
-    event.value = event.message.substring(event.message.indexOf('(') + 1, event.message.indexOf('minute') - 4);
-  } else if(event.message.includes("Fin de période")) {
+    event.value = message.substring(message.indexOf('(') + 1, message.indexOf('minute') - 4);
+  } else if(message.includes("Fin de période")) {
     event.eventType = EventType.END_PERIOD;
-  } else if(event.message.includes("Fin du match")) {
+  } else if(message.includes("Fin du match")) {
     event.eventType = EventType.END_MATCH;
   } else {
     event.eventType = EventType.UNSUPPORTED_TYPE;
